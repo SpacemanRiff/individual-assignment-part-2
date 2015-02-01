6b217327ae4940c3ddfae146b2c5cb6844fc7e50
Scott Troutman <troutmansm@gmail.com>
2015-01-31 21:04:01 -0600

    Started the User Interface
    Got it to display, the board, but it ain't pretty.

--- src/main/java/edu/jsu/mcis/TicTacToeInterface.java
+++ src/main/java/edu/jsu/mcis/TicTacToeInterface.java
@@ -1,93 +1,64 @@
 package edu.jsu.mcis;
+  
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
 
-public class TicTacToeInterface{		
+public class TicTacToeInterface extends JPanel{		
 	private TicTacToe game;
-	private String previousPlayer;
+	public JButton[][] ticTacToeButtons;
+	public JLabel currentTurnLabel;
 	
 	public TicTacToeInterface(){		
 		game = new TicTacToe();
-		previousPlayer = " ";
-	}
-	
-	public void playGame(){		
-		while(!game.isTheGameOver()){
-			getUserInput();
-		}
-		if(!game.whoIsTheWinner().equals(game.EMPTY_SPACE_SYMBOL)){
-			System.out.println(whoWon() + " is the Winner!");
-		}else{
-			System.out.println(whoWon() + " game!");
-		}
-	}
-	
-	public void getUserInput(){
-		Scanner input = new Scanner(System.in);
-		int rowInput = -1; 
-		int columnInput = -1;
-		if(previousPlayer.equals(game.whoseTurnIsIt())){			
-			System.out.println("That space was not available.");
-		}
+		ticTacToeButtons = new JButton[3][3];
 		
-		System.out.println("It is player " + game.whoseTurnIsIt() + "\'s turn.");
-		System.out.print("Please enter your desired row: ");
-		while(rowInput == -1){
-			try{
-				rowInput = Integer.parseInt(input.next());
-			}catch(Exception ex){
-				System.out.print("Invalid input. Please try again: ");
+		setLayout(new GridLayout(3,3));
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				ticTacToeButtons[i][j] = new JButton(game.whoControlsTheTileAt(i,j));
+				ticTacToeButtons[i][j].addActionListener(new ButtonListener(i,j));
+				ticTacToeButtons[i][j].setName("Location" + i + j);
+				add(ticTacToeButtons[i][j]);
 			}
 		}
-		
-		System.out.print("Please enter your desired column: ");
-		while(columnInput == -1){
-			try{
-				columnInput = input.nextInt();
-			}catch(Exception ex){
-				System.out.print("Invalid input. Please try again: ");
-			}
-		}
-
-		previousPlayer = game.whoseTurnIsIt();
-		
-		sendDataToBoard(rowInput, columnInput);
 	}
 	
 	public void sendDataToBoard(int row, int column){
 		game.markLocation(row, column);
-		drawBoard();        
-        game.checkToSeeIfTheGameIsOver();
 	}
 	
 	public String whoControlsThisTile(int row, int column){
-		return game.whoControlsTheTileAt(row, column);
+		return ticTacToeButtons[row][column].getText();
 	}
 	
-	public String whoWon(){
-		return game.whoIsTheWinner();
-	}
-	
-	public void drawBoard(){
-		System.out.println();
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-				System.out.print(whoControlsThisTile(i,j));
-				if(j != 2){
-					System.out.print("|");
-				}
-			}
-			System.out.println();
-			if(i != 2){
-				System.out.println("-----");
-			}
-		}
-		System.out.println();
-	}
-	
-	public static void main(String[] args){
-		System.out.println();
+	public static void main(String[] args){		
 		TicTacToeInterface gameInterface = new TicTacToeInterface();
-		gameInterface.playGame();
+		JFrame frame = new JFrame("Tic Tac Toe :^)");
+		frame.add(gameInterface);		
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setSize(600,600);
+		frame.setVisible(true);
+	}
+	
+	public class ButtonListener implements ActionListener{
+		int row, column;
+		public ButtonListener(int row, int column){
+			this.row = row;
+			this.column = column;
+		}
+		
+		public void actionPerformed(ActionEvent e){
+			sendDataToBoard(row, column);
+			ticTacToeButtons[row][column].setText(game.whoControlsTheTileAt(row,column));
+		}
 	}
 }
--- src/test/java/edu/jsu/mcis/TicTacToeInterfaceTest.java
+++ src/test/java/edu/jsu/mcis/TicTacToeInterfaceTest.java
@@ -27,41 +27,4 @@ public class TicTacToeInterfaceTest{
 		gameInterface.sendDataToBoard(0,0);
 		assertEquals("X",gameInterface.whoControlsThisTile(0,0));
 	}	
-	@Test
-	public void testIfXHasWonThroughTheInterface(){
-		TicTacToeInterface gameInterface = new TicTacToeInterface();		
-		gameInterface.sendDataToBoard(0,1);
-		gameInterface.sendDataToBoard(0,0);		
-		gameInterface.sendDataToBoard(1,1);
-		gameInterface.sendDataToBoard(0,2);
-		gameInterface.sendDataToBoard(2,1);
-		assertEquals("X",gameInterface.whoWon());
-	}
-	
-	@Test
-	public void testColumnThreeWin(){	
-		TicTacToeInterface gameInterface = new TicTacToeInterface();	
-		gameInterface.sendDataToBoard(1,1);
-		gameInterface.sendDataToBoard(0,2);
-		gameInterface.sendDataToBoard(0,0);
-		gameInterface.sendDataToBoard(1,2);
-		gameInterface.sendDataToBoard(0,1);
-		gameInterface.sendDataToBoard(2,2);
-		assertEquals("O",gameInterface.whoWon());
-	}
-	
-	@Test
-	public void testIsTheBoardFullThroughTheInterface(){		
-		TicTacToeInterface gameInterface = new TicTacToeInterface();		
-		gameInterface.sendDataToBoard(0,0);			
-		gameInterface.sendDataToBoard(0,1);	
-		gameInterface.sendDataToBoard(0,2);	
-		gameInterface.sendDataToBoard(2,0);			
-		gameInterface.sendDataToBoard(2,1);	
-		gameInterface.sendDataToBoard(2,2);
-		gameInterface.sendDataToBoard(1,0);			
-		gameInterface.sendDataToBoard(1,1);	
-		gameInterface.sendDataToBoard(1,2);
-		assertEquals("TIE",gameInterface.whoWon());
-	}
 }
